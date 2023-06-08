3b46a12964dbb3f2c8c6a31912d6ce1251945219
Alba Vargas <118747197+albavargasnav@users.noreply.github.com>
2023-06-08 22:43:38 +0200

    Add Root

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import { setAuthorizationHeader } from './api/client';
 
 import configureStore from './redux';
 
-import { BrowserRouter } from 'react-router-dom';
 import { AuthContextProvider } from './components/auth/context';
+import Root from './Root';
 
 const accessToken = storage.get('auth');
 if (accessToken) {
@@ -21,10 +21,10 @@ const store = configureStore()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Root store={store}>
       <AuthContextProvider isInitiallyLogged={!!accessToken}>
-        <App />
+          <App />
       </AuthContextProvider>
-    </BrowserRouter>
+    </Root>
   </React.StrictMode>
 );
