69b6c1367994b40cd39b5210e1aa6302108a0a39
Alba Vargas <118747197+albavargasnav@users.noreply.github.com>
2023-07-09 23:30:31 +0200

    Add test async

--- src/store/__tests__/actions.js
+++ src/store/__tests__/actions.js
@@ -1,4 +1,11 @@
-import { advertsLoadedSuccess, authLogoutSuccess } from '../actions';
+import {
+  advertsLoadedSuccess,
+  authLogoutSuccess,
+  authLogin,
+  authLoginRequest,
+  authLoginSuccess,
+  authLoginFailure,
+} from '../actions';
 
 import { ADVERTS_LOADED_SUCCESS, AUTH_LOGOUT } from '../types';
 
@@ -20,5 +27,39 @@ describe('authLogoutSuccess', () => {
       type: AUTH_LOGOUT,
     };
     expect(authLogoutSuccess()).toEqual(expectedAction);
+  });
+});
+
+describe('authLogin', () => {
+  const credentials = 'credentials';
+  const redirectUrl = 'redirectUrl';
+  const action = authLogin(credentials);
+
+  const dispatch = jest.fn();
+  const service = { auth: {} };
+  const router = {
+    navigate: jest.fn(),
+    state: { location: { state: { from: { pathname: redirectUrl } } } },
+  };
+
+  test('when login api resolves should follow the login flow', async () => {
+    // service.auth.login = jest.fn().mockImplementation(() => Promise.resolve());
+    service.auth.login = jest.fn().mockResolvedValue();
+    await action(dispatch, undefined, { service, router });
+
+    expect(dispatch).toHaveBeenNthCalledWith(1, authLoginRequest());
+    expect(service.auth.login).toHaveBeenCalledWith(credentials);
+    expect(dispatch).toHaveBeenNthCalledWith(2, authLoginSuccess());
+    expect(router.navigate).toHaveBeenCalledWith(redirectUrl);
+  });
+
+  test('when login api rejects should follow error flow', async () => {
+    const error = new Error('unauthorized');
+    service.auth.login = jest.fn().mockRejectedValue(error);
+    await action(dispatch, undefined, { service });
+
+    expect(dispatch).toHaveBeenNthCalledWith(1, authLoginRequest());
+    expect(service.auth.login).toHaveBeenCalledWith(credentials);
+    expect(dispatch).toHaveBeenNthCalledWith(2, authLoginFailure(error));
   });
 });
